bc8448055bfff2aebad3e8a409feca9e22ed6c41
Takashi Nakagawa <tak1240@gmail.com>
2015-01-08 03:22:49 +0900

    tryout service worker

---
+++ tryout-sw/sw.js
@@ -0,0 +1,58 @@
+// import polyfill like webworker
+importScripts('/node_modules/serviceworker-cache-polyfill/dist/serviceworker-cache-polyfill.js');
+
+// prepare to cache
+var CACHE_NAME = 'my-test-site-cache-v1';
+var urlToCache = [
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return caches.addAll(urlToCache);
+      });
+  );
+});
+
+
+// cache requested response
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // キャッシュがあったのでレスポンスを返す
+        if (response) {
+          return response;
+        }
+
+        // 重要：リクエストを clone する。リクエストは Stream なので
+        // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+        // 必要なので、リクエストは clone しないといけない
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then(function(response) {
+            // レスポンスが正しいかをチェック
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 重要：レスポンスを clone する。レスポンスは Stream で
+            // ブラウザ用とキャッシュ用の2回必要。なので clone して
+            // 2つの Stream があるようにする
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                var cacheRequest = event.request.clone();
+                cache.put(cacheRequest, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
