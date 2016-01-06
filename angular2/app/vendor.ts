d6fdf547d36686cd1e7f4c4ee080228b2351d759
Takashi Nakagawa <tak1240@gmail.com>
2016-01-06 21:58:17 +0900

    init config with webpack

---
+++ angular2/app/vendor.ts
@@ -0,0 +1,14 @@
+// Polyfills
+import 'es6-promise';
+import 'es6-shim';
+import 'reflect-metadata';
+import 'zone.js/lib/browser/zone-microtask';
+// Angular 2
+import 'angular2/platform/browser';
+import 'angular2/platform/common_dom';
+import 'angular2/router';
+import 'angular2/http';
+import 'angular2/core';
+
+// RxJS
+import 'rxjs';
---
+++ angular2/webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+
+var configurations = {
+  entry: {
+    vendor: './app/vendor.ts',
+    app: './app/boot.ts'
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  resolve: {
+    // ensure loader extensions match
+    extensions: ['','.ts','.js','.json','.css','.html']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        query: {
+          'ignoreDiagnostics': [
+            2403, // 2403 -> Subsequent variable declarations
+            2300, // 2300 -> Duplicate identifier
+            2374, // 2374 -> Duplicate number index signature
+            2375  // 2375 -> Duplicate string index signature
+          ]
+        },
+        exclude: [ /node_modules/ ]
+      },
+    ]
+  },
+}
+
+module.exports = configurations
